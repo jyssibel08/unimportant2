e1459bf04fdcda7ab59566ad2f829dee23dc9b58
jyssibel08 <95346666+jyssibel08@users.noreply.github.com>
2022-05-12 20:46:00 +0800

    Update 10.sh

--- 10.sh
+++ 10.sh
@@ -607,24 +607,11 @@ read -rp "Please select VPS password：" menu_num1
 read -p "Enter Panel Host: "  db_hostname
 read -p "Enter Panel location: "  db_location
 read -p "Enter Panel flag: "  db_flag
-    echo -e "—————————————— Select Panel ——————————————"""
-    echo -e "${Green}1.${Font}  XAMJYSSVPN"
-    echo -e "${Green}2.${Font}  CoronaSSH \n"
-    
-read -rp "Please select Panel：" menu_num
-    case $menu_num in
-    1)
 	mysql -uxamjyssvpn -pXamjyss14302082020! -h162.216.115.91 -e"USE xamjyssvpn
 	INSERT INTO servers (id, host, host_address, type, ws_dns, ns, chave, root_password, location, flag) VALUES ('', '$db_hostname', '$IPADDR', 'ssh', '$MYDNS', 'none', 'none', '$db_root_password', '$db_location', '$db_flag');"
-        ;;
-    2)
+	
 	mysql -ucoronassh.com -pXamjyss14302082020! -h162.216.115.91 -e"USE coronassh.com
 	INSERT INTO servers (id, host, host_address, type, ws_dns, ns, chave, root_password, location, flag) VALUES ('', '$db_hostname', '$IPADDR', 'ssh', '$MYDNS', 'none', 'none', '$db_root_password', '$db_location', '$db_flag');"
-	;;
-    *)
-        echo -e "${RedBG}Please enter the correct number ${Font}"
-        ;;
-    esac
 
 bash /etc/profile.d/bonv.sh
 systemctl enable openvpn
